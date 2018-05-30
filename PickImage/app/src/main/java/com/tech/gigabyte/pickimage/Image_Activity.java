50cfc5ed24b58022e3c84268ffbed603981efc2f
Ashutosh Dubey <ashutosh00074@gmail.com>
2018-05-31 01:38:46 +0530

    Initial commit

---
+++ PickImage/app/src/main/java/com/tech/gigabyte/pickimage/Image_Activity.java
@@ -0,0 +1,102 @@
+package com.tech.gigabyte.pickimage;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.FileDescriptor;
+import java.io.IOException;
+
+public class Image_Activity extends AppCompatActivity {
+
+
+    private static int RESULT_LOAD_IMAGE = 1;
+
+    /**
+     * Called when the activity is first created.
+     */
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image);
+
+        Button buttonLoadImage = (Button) findViewById(R.id.buttonLoadPicture);
+        buttonLoadImage.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent i = new Intent(
+                        Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+                startActivityForResult(i, RESULT_LOAD_IMAGE);
+            }
+        });
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Check which request responding to Make sure the request was successful
+
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) { // The user picked a Image
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+
+            //using CursorLoader to perform the query.
+
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            assert cursor != null;
+            cursor.moveToFirst();
+
+            // Retrieve the Image from the Gallery
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+
+            ImageView imageView = (ImageView) findViewById(R.id.imgView);
+
+            Bitmap bmp = null;
+            try {
+                bmp = getBitmapFromUri(selectedImage);
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            imageView.setImageBitmap(bmp);
+
+        }
+
+
+    }
+
+    //to create a FileInputStream or FileOutputStream to contain it
+
+
+    private Bitmap getBitmapFromUri(Uri uri) throws IOException {
+        ParcelFileDescriptor parcelFileDescriptor =
+                getContentResolver().openFileDescriptor(uri, "r");
+        assert parcelFileDescriptor != null;
+        FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
+        Bitmap image = BitmapFactory.decodeFileDescriptor(fileDescriptor);
+        parcelFileDescriptor.close();
+        return image;
+    }
+
+
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# ACADGILD-Session7-Assignment7.3
+Assignment 7.3: Upload Image from Gallery and Show in ImageView.
+
+Simply a Image pick app, User can upload any image from Gallery 
+
+Concept: Intent is a simple message object which is used to communicate from one activity to another activity 
+
+Problem Statement
+You need to upload Image from Gallery and show it in imageview.
+
+Expected Output
+An imageview showing image uploaded from Gallery.
